329bcdce0499683b7cc24663f18839b18b34ec9c
Mike Mathew <m2mathew@me.com>
2018-02-12 23:48:19 -0600

    adds shared sponsor-card

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,5 +21,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`
   ],
 };
---
+++ src/components/shared/sponsor-card/index.js
@@ -0,0 +1,25 @@
+// External Dependencies
+import React from "react"
+import Link from "gatsby-link"
+import styled from "styled-components";
+// import Img from "gatsby-image"
+
+// Internal Dependencies
+import typography, { rhythm, scale } from "../utils/typography"
+import presets, { colors } from "../utils/presets"
+
+// Local Variables
+const SponsorCardWrapper = styled.div`
+  margin-bottom: 1rem;
+  padding: 2rem 3rem;
+  background: white;
+  border-radius: 4px;
+  box-shadow: rgba(25, 17, 34, 0.05) 0px 3px 10px;
+`;
+
+// Component Definition
+export default () => (
+  <div>
+
+  </div>
+);
