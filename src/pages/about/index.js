3e1efcac87fd86b409ee16cdf3d534f8bd886e3e
Mike Mathew <m2mathew@me.com>
2018-03-07 22:17:35 -0600

    [about] Provides divider for separation of content and sidebar on mobile

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import Link from 'gatsby-link';
 import Helmet from 'react-helmet';
+import hex2rgba from 'hex2rgba';
 
 // Internal Dependencies
 import Container from '../../components/shared/container';
@@ -10,6 +11,9 @@ import SidebarBody from '../../components/shared/sidebar/sidebar-body';
 
 // Sidebar data
 import aboutSidebar from './about-links.yml';
+
+// Local Variables
+const texasFlagRed = '#BF0A30';
 
 // Component Definition
 export default ({ data }) => (
@@ -87,6 +91,11 @@ export default ({ data }) => (
           },
         }}
       >
+        <hr css={{
+          height: 6,
+          border: 0,
+          boxShadow: `inset 0 12px 12px -12px ${hex2rgba(texasFlagRed, 0.9)}`,
+        }} />
         <h2>All About TMAC</h2>
         <SidebarBody inline yaml={aboutSidebar} />
       </div>
