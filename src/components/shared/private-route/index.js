2462ead5def8d315ec6cdb7cef71fccacbde42c6
Mike Mathew <m2mathew@me.com>
2018-03-30 00:26:25 -0500

    [npm] Adds react-router-dom and some auth files

---
+++ src/components/shared/private-route/index.js
@@ -0,0 +1,22 @@
+// External Dependencies
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+
+// Internal Dependencies
+import { isLoggedIn } from '../../../utils/auth';
+
+// More info at https://reacttraining.com/react-router/web/example/auth-workflow
+// Component Definition
+export default ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      !isLoggedIn() ? (
+        // If we’re not logged in, redirect to the home page.
+        <Redirect to={{ pathname: '/app/login' }} />
+      ) : (
+        <Component {...props} />
+      )
+    }
+  />
+);
