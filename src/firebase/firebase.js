f8343f7a6986db4cfcae8e54779e68772b19468c
Mike Mathew <m2mathew@me.com>
2018-04-03 06:29:09 -0500

    [firebase] Moves firebase to its own folder

---
+++ src/firebase/firebase.js
@@ -0,0 +1,38 @@
+import * as firebase from 'firebase';
+
+// https://firebase.google.com/docs/web/setup?authuser=0
+
+// TODO: Set up a separate prod config
+// const prodConfig = {
+//   apiKey: process.env.GATSBY_FIREBASE_API_KEY,
+//   authDomain: process.env.GATSBY_FIREBASE_AUTH_DOMAIN,
+//   databaseURL: process.env.GATSBY_FIREBASE_DATABASE_URL,
+//   projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.GATSBY_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.GATSBY_FIREBASE_MESSAGING_SENDER_ID,
+// };
+
+const devConfig = {
+  apiKey: process.env.GATSBY_FIREBASE_API_KEY,
+  authDomain: process.env.GATSBY_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.GATSBY_FIREBASE_DATABASE_URL,
+  projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.GATSBY_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.GATSBY_FIREBASE_MESSAGING_SENDER_ID,
+};
+
+// const config = process.env.NODE_ENV === 'production'
+//   ? prodConfig
+//   : devConfig;
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(devConfig);
+}
+
+const db = firebase.database();
+const auth = firebase.auth();
+
+export {
+  db,
+  auth,
+};
---
+++ src/firebase/index.js
@@ -0,0 +1,12 @@
+// As found in this GitHub repo:
+// https://github.com/rwieruch/gatsby-firebase-authentication
+
+import * as auth from './auth';
+import * as db from './db';
+import * as firebase from './firebase';
+
+export {
+  auth,
+  db,
+  firebase,
+};
