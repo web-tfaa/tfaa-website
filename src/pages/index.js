81b5a3cc16892c27d6dbeea4a6bde470a5806a9c
Mike Mathew <m2mathew@me.com>
2018-04-08 17:48:46 -0500

    [Home] Aligns font for president's message

--- src/pages/index.js
+++ src/pages/index.js
@@ -62,8 +62,11 @@ export default (props) => (
             If you are a music/fine arts administrator, an aspiring administrator, or if you are responsible for the organizing or supervision of music activities in your district, we encourage you to become a member of TMAC. We will be hosting a Summer Round Table session during the summer TBA/TCDA/TODA conferences in San Antonio on Friday, July 21, 2017 - everyone is invited to attend! Our Fall Conference, for TMAC members only, will be held November 15-17, 2017 in Austin, Texas. If you would like more information on registering for the Fall Conference, please contact a TMAC Officer.
           </FuturaParagraph>
 
-          <div><a href="mailto:johnjanda@tomballisd.net">JD Janda</a>, President</div>
-          <div>Texas Music Administrators Conference</div>
+          <FuturaParagraph>
+            <a href="mailto:johnjanda@tomballisd.net">JD Janda</a>, President
+            <br />
+            Texas Music Administrators Conference
+          </FuturaParagraph>
         </Card>
       </Cards>
 
