2fc50e576b3b3f10a30eb104802065ce1f961282
Mike Mathew <m2mathew@me.com>
2018-09-16 21:22:58 -0500

    [firebase] Adds prod keys

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,14 +5,14 @@ import 'firebase/auth';
 // https://firebase.google.com/docs/web/setup?authuser=0
 
 // TODO: Set up a separate prod config
-// const prodConfig = {
-//   apiKey: process.env.GATSBY_FIREBASE_API_KEY,
-//   authDomain: process.env.GATSBY_FIREBASE_AUTH_DOMAIN,
-//   databaseURL: process.env.GATSBY_FIREBASE_DATABASE_URL,
-//   projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.GATSBY_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.GATSBY_FIREBASE_MESSAGING_SENDER_ID,
-// };
+const prodConfig = {
+  apiKey: process.env.GATSBY_FIREBASE_API_KEY_PROD,
+  authDomain: process.env.GATSBY_FIREBASE_AUTH_DOMAIN_PROD,
+  databaseURL: process.env.GATSBY_FIREBASE_DATABASE_URL_PROD,
+  projectId: process.env.GATSBY_FIREBASE_PROJECT_ID_PROD,
+  storageBucket: process.env.GATSBY_FIREBASE_STORAGE_BUCKET_PROD,
+  messagingSenderId: process.env.GATSBY_FIREBASE_MESSAGING_SENDER_ID_PROD,
+};
 
 const devConfig = {
   apiKey: process.env.GATSBY_FIREBASE_API_KEY,
@@ -23,12 +23,12 @@ const devConfig = {
   storageBucket: process.env.GATSBY_FIREBASE_STORAGE_BUCKET,
 };
 
-// const config = process.env.NODE_ENV === 'production'
-//   ? prodConfig
-//   : devConfig;
+const config = process.env.NODE_ENV === 'production'
+  ? prodConfig
+  : devConfig;
 
 if (!firebase.apps.length) {
-  firebase.initializeApp(devConfig);
+  firebase.initializeApp(config);
 }
 
 // eslint-disable-next-line
