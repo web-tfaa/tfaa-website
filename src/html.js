da2d8ec1e8aafc820f9f51536c21f4b921fcd294
Mike Mathew <m2mathew@me.com>
2018-09-18 07:20:19 -0500

    Adds google verify meta tag

--- src/html.js
+++ src/html.js
@@ -62,6 +62,7 @@ export default class HTML extends React.Component {
             src="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.js"
             defer
           />
+          <meta name="google-site-verification" content="zatUmTHTSD3gJPg0iwPub6EGhF5De-kqJHy826gYNh0" />
         </head>
         <body {...this.props.bodyAttributes}>
           <div
