7743a01ab924a4d133ccce65da4da7ae56e8c289
Mike Mathew <m2mathew@me.com>
2019-01-14 09:50:54 -0600

    Removes gatsby-browser.js code

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,7 @@
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 
-export const replaceHydrateFunction = () => {
-  return (element, container, callback) => {
-    ReactDOM.render(element, container, callback);
-  };
-};
+// export const replaceHydrateFunction = () => {
+//   return (element, container, callback) => {
+//     ReactDOM.render(element, container, callback);
+//   };
+// };
--- src/components/shared/futura-div.js
+++ src/components/shared/futura-div.js
@@ -1,41 +1,26 @@
 // Internal Dependencies
 import PropTypes from 'prop-types';
 import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
-
-// Internal Dependencies
 import { options } from '../../utils/typography';
 
-// Local Variables
-const propTypes = {
-  children: PropTypes.oneOfType([
-    PropTypes.array,
-    PropTypes.shape({}),
-    PropTypes.string,
-  ]).isRequired,
-  classes: PropTypes.shape({
-    root: PropTypes.string.isRequired,
-  }).isRequired,
-};
-
-const styles = {
-  root: {
-    fontFamily: options.headerFontFamily.join(','),
-    lineHeight: '1.6',
-    marginBottom: '1rem',
-  },
-};
-
 // Component Definition
-const FuturaDiv = ({
-  children,
-  classes,
-}) => (
-  <div className={classes.root}>
+const FuturaDiv = ({ children }) => (
+  <div
+    css={{
+      fontFamily: options.headerFontFamily.join(','),
+      lineHeight: '1.6',
+      marginBottom: '1rem',
+    }}
+  >
     {children}
   </div>
 );
 
-FuturaDiv.propTypes = propTypes;
-
-export default withStyles(styles)(FuturaDiv);
+FuturaDiv.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.array,
+    PropTypes.shape({}),
+  ]).isRequired,
+}
+export default FuturaDiv;
