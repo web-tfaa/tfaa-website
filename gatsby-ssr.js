9777ba3d4b5943447c206de4d98d709c2aa9b57b
Mike Mathew <m2mathew@me.com>
2019-01-14 18:57:59 -0600

    [MUI] Trying to undo the flash of no CSS in prod

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,41 +1,41 @@
-/* eslint-disable react/no-danger */
+// /* eslint-disable react/no-danger */
 
-const React = require('react');
-const { renderToString } = require('react-dom/server');
-const { JssProvider } = require('react-jss');
-const getPageContext = require('./src/utils/getPageContext').default;
+// const React = require('react');
+// const { renderToString } = require('react-dom/server');
+// const { JssProvider } = require('react-jss');
+// const getPageContext = require('./src/utils/getPageContext').default;
 
-function replaceRenderer({
-  bodyComponent,
-  replaceBodyHTMLString,
-  setHeadComponents,
-}) {
-  // Get the context of the page to collected side effects.
-  const pageContext = getPageContext();
+// function replaceRenderer({
+//   bodyComponent,
+//   replaceBodyHTMLString,
+//   setHeadComponents,
+// }) {
+//   // Get the context of the page to collected side effects.
+//   const pageContext = getPageContext();
 
-  const bodyHTML = renderToString(
-    <JssProvider
-      generateClassName={pageContext.generateClassName}
-      registry={pageContext.sheetsRegistry}
-    >
-      {bodyComponent}
-    </JssProvider>,
-  );
+//   const bodyHTML = renderToString(
+//     <JssProvider
+//       generateClassName={pageContext.generateClassName}
+//       registry={pageContext.sheetsRegistry}
+//     >
+//       {bodyComponent}
+//     </JssProvider>,
+//   );
 
-  replaceBodyHTMLString(bodyHTML);
-  setHeadComponents([
-    <style
-      type="text/css"
-      id="jss-server-side"
-      key="jss-server-side"
-      dangerouslySetInnerHTML={{
-        __html: pageContext.sheetsRegistry.toString(),
-      }}
-    />,
-  ]);
-}
+//   replaceBodyHTMLString(bodyHTML);
+//   setHeadComponents([
+//     <style
+//       type="text/css"
+//       id="jss-server-side"
+//       key="jss-server-side"
+//       dangerouslySetInnerHTML={{
+//         __html: pageContext.sheetsRegistry.toString(),
+//       }}
+//     />,
+//   ]);
+// }
 
-exports.replaceRenderer = replaceRenderer;
+// exports.replaceRenderer = replaceRenderer;
 
 // It's not ready yet: https://github.com/gatsbyjs/gatsby/issues/8237.
 //
