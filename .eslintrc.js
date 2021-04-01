76861a62410355ec4ec9a678fb463d44304d73be
Michael Mathew <m2mathew@me.com>
2021-03-31 19:29:24 -0500

    chore: update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,50 +1,69 @@
-{
+module.exports = {
   extends: [
-    // 'airbnb'
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
+    'airbnb',
+    'plugin:@typescript-eslint/recommended'
   ],
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
-    'react/jsx-filename-extension': [
-      error,
-      { extensions: ['.js', '.jsx'] }
+    'arrow-body-style': 0,
+    'comma-dangle': 0,
+    'consistent-return': 0,
+    'implicit-arrow-linebreak': 0,
+    'import/extensions': [
+      0,
+      'ignorePackages',
+      {
+        js: 'never',
+        mjs: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
     ],
-    'react/react-in-jsx-scope': 0,
-    'react/require-default-props': 0,
+    'import/no-unresolved': 0,
+    'import/prefer-default-export': 0,
+    'jsx-a11y/anchor-is-valid': 0,
+    'no-case-declarations': 0,
+    'no-console': 0,
+    'no-nested-ternary': 0,
+    'no-shadow': 0,
+    'no-underscore-dangle': 0,
     'react/forbid-prop-types': 1,
+    'react/jsx-filename-extension': 'off',
+    'react/jsx-one-expression-per-line': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/jsx-uses-vars': [2],
     'react/no-did-mount-set-state': 0,
     'react/prefer-stateless-function': 1,
     'react/prop-types': 'off', // Disable prop-types as we use TypeScript for type checking
-    'react/jsx-uses-vars': [2],
-    'react/jsx-one-expression-per-line': 0,
-    'jsx-a11y/anchor-is-valid': 0,
-    'no-underscore-dangle': 0,
-    'arrow-body-style': 0,
-    'comma-dangle': 1,
-    'no-shadow': 0,
-    'consistent-return': 0,
-    'no-nested-ternary': 0,
-    'no-console': 0,
-    'no-case-declarations': 0,
-    'import/prefer-default-export': 0,
-    'import/no-unresolved': 0,
-    'implicit-arrow-linebreak': 0,
-    'react/jsx-props-no-spreading': 0
+    'react/react-in-jsx-scope': 0,
+    'react/require-default-props': 0,
   },
   settings: {
     // 'import/core-modules': ['react', 'prop-types']
     react: {
       version: 'detect'
-    },
+    }
   },
   globals: {
     graphql: true
   },
-  plugins: ['@typescript-eslint', 'react'],
+  overrides: [
+    {
+      files: ['*test.js', '*.spec.js'],
+      rules: {
+        'no-undef': 0,
+      },
+    },
+  ],
+  plugins: [
+    '@typescript-eslint',
+    'typescript',
+    'react',
+    'react-hooks',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2018,
@@ -58,4 +77,4 @@
     es6: true,
     node: true
   }
-}
+};
