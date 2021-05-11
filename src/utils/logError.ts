5f0a892de84d6b9b610d2e90717c617bcf439d80
Michael Mathew <m2mathew@me.com>
2021-05-10 21:25:47 -0500

    chore: add a function to log errors

---
+++ src/utils/logError.ts
@@ -0,0 +1,24 @@
+// External Dependencies
+import * as Sentry from '@sentry/gatsby';
+
+export const logError = (
+  errorMessage: string,
+  originalError: Error
+): void => {
+  // Let's not log errors to Sentry if we are testing
+  if (process.env.NODE_ENV !== 'test') {
+    console.error(errorMessage);
+
+    // The Error constructor always has a "message"
+    console.error(originalError.message);
+
+    console.error(JSON.stringify(originalError));
+
+    // We send to Sentry, which also captures what happened just before this
+    Sentry.captureException(originalError, {
+      extra: {
+        customMessage: errorMessage,
+      },
+    });
+  }
+};
