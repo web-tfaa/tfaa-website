1366a062ad368df8aa91240d1ed8d579ea7e6446
Mike Mathew <m2mathew@me.com>
2022-11-20 12:42:04 -0600

    refactor: convert invoice-table to use emotion styled, remove CustomTableCell

--- src/components/register/invoice-table.js
+++ src/components/register/invoice-table.js
@@ -3,38 +3,57 @@ import {
   Paper,
   Table,
   TableBody,
+  TableCell,
   TableContainer,
   TableHead,
   TableRow,
 } from '@mui/material';
 import PropTypes from 'prop-types';
-import React from 'react';
-import { makeStyles } from '@mui/styles';
+import React, { useMemo } from 'react';
+import styled from '@emotion/styled';
 
 // Internal Dependencies
-import CustomTableCell from '../shared/CustomTableCell';
 import { currentSchoolYearLong } from '../../utils/helpers';
 
 // Local Variables
-const useStyles = makeStyles((theme) => ({
-  root: {
-    marginLeft: 32,
-    marginRight: 32,
-    marginTop: theme.spacing(2),
-    width: '75%',
+const StyledTableContainer = styled(TableContainer)(({ theme }) => ({
+  '.body-cell': {
+    fontSize: 14,
+    '&:first-child': {
+      paddingLeft: theme.spacing(2),
+    },
+    '&:nth-child(2)': {
+      paddingRight: theme.spacing(2),
+    },
   },
-  table: {
+  '.header-cell': {
+    backgroundColor: '#EDF2F8',
+    color: theme.palette.common.black,
+    fontWeight: 600,
+    '&:first-child': {
+      paddingLeft: theme.spacing(2),
+    },
+    '&:nth-child(2)': {
+      paddingRight: theme.spacing(2),
+    },
+  },
+  '.invoice-table': {
     border: `2px solid ${theme.palette.grey[600]}`,
     marginBottom: 0,
     minWidth: 200,
     paddingRight: theme.spacing(1),
   },
-  row: {
+  '.invoice-row': {
     '&:nth-of-type(odd)': {
       backgroundColor: theme.palette.background.default,
     },
     paddingRight: theme.spacing(1),
   },
+
+  marginLeft: 32,
+  marginRight: 32,
+  marginTop: theme.spacing(2),
+  width: '75%',
 }));
 
 // Component Definition
@@ -45,55 +64,62 @@ const InvoiceTable = ({
   isInvoice,
   sponsorLevel,
 }) => {
-  const classes = useStyles();
-
   // Work out the correct amount
   const isString = typeof amount === 'string';
   const updatedAmount = amount === 0 ? 50 : amount;
 
-  const sponsorInfo = (
+  const sponsorInfo = useMemo(() => (
     <span>
       TMAC {sponsorLevel} Sponsor donation amount{' '}
       <br />for <strong>{form.SponsorOrganization}</strong>
     </span>
-  );
+  ), [form.SponsorOrganization, sponsorLevel]);
 
-  const memberInfo = (
+  const memberInfo = useMemo(() => (
     <span>
       TMAC {isActive || !form.MemberType ? 'Active' : 'Retired'} membership fee{' '}
       <br />for <strong>{form.FirstName} {form.LastName}</strong>
     </span>
-  );
+  ), [form.FirstName, form.LastName, form.MemberType, isActive]);
 
   return (
-    <TableContainer
-      className={classes.root}
-      component={Paper}
-    >
-      <Table className={classes.table}>
+    <StyledTableContainer component={Paper}>
+      <Table className="invoice-table">
         <TableHead>
           <TableRow>
-            <CustomTableCell>Description</CustomTableCell>
-            <CustomTableCell align="right">
+            <TableCell className="header-cell">Description</TableCell>
+
+            <TableCell
+              align="right"
+              className="header-cell"
+            >
               Total{!isInvoice && ' Paid'}
-            </CustomTableCell>
+            </TableCell>
           </TableRow>
         </TableHead>
 
         <TableBody>
-          <TableRow className={classes.row}>
-            <CustomTableCell component="th" scope="row">
+          <TableRow className="invoice-row">
+            <TableCell
+              className="body-cell"
+              component="th"
+              scope="row"
+            >
               {sponsorLevel ? sponsorInfo : memberInfo}
               <br />
               for the {currentSchoolYearLong} school year.
-            </CustomTableCell>
-            <CustomTableCell align="right">
+            </TableCell>
+
+            <TableCell
+              align="right"
+              className="body-cell"
+            >
               {isString ? updatedAmount : `$${updatedAmount?.toFixed(2).toLocaleString()}`}
-            </CustomTableCell>
+            </TableCell>
           </TableRow>
         </TableBody>
       </Table>
-    </TableContainer>
+    </StyledTableContainer>
   );
 };
 
