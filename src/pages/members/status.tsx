96fc07c7bbeb4b47bdf0920bd2390ffb2786c7e3
Mike Mathew <m2mathew@me.com>
2022-11-20 19:24:49 -0600

    refactor(status): convert to TS and styled-components

--- src/pages/members/status.tsx
+++ src/pages/members/status.tsx
@@ -1,70 +1,64 @@
 // External Dependencies
 import { Link } from 'gatsby-theme-material-ui';
 import { Typography } from '@mui/material';
-import { makeStyles } from '@mui/styles';
-import PropTypes from 'prop-types';
-import React from 'react';
+import { FC } from 'react';
+import styled from 'styled-components';
 
 // Internal Dependencies
 import AuthUserContext from '../../components/session/AuthUserContext';
 import { colors } from '../../utils/presets';
 import { auth } from '../../firebase';
 
+// Local Typings
+interface Props {
+  authUser: firebase.User;
+}
+
 // Local Variables
-const propTypes = {
-  authUser: PropTypes.shape({
-    email: PropTypes.string,
-    uid: PropTypes.string,
-  }),
-};
-
-const defaultProps = {
-  authUser: null,
-};
-
-const useStyles = makeStyles((theme) => ({
-  anchor: {
+const StyledRoot = styled.section(({ theme }) => ({
+  '.anchor': {
     marginLeft: theme.spacing(1.5),
   },
-  root: {
-    background: colors.status,
-    display: 'flex',
-    fontSize: '87.5%',
-    height: theme.spacing(6),
-    justifyContent: 'flex-end',
-    padding: theme.spacing(1),
-  },
-  text: {
+
+  '.text': {
     fontSize: '0.9rem',
   },
+
+  background: colors.status,
+  display: 'flex',
+  alignItems: 'center',
+  fontSize: '87.5%',
+  height: theme.spacing(6),
+  justifyContent: 'flex-end',
+  padding: theme.spacing(1),
 }));
 
 // Component Definition
-const Status = ({ authUser }) => {
-  const classes = useStyles();
-
+const Status: FC<Props> = ({ authUser }) => {
   const isAuthenticated = Boolean(authUser);
 
   const details = !isAuthenticated ? (
-    <Typography className={classes.text}>
+    <Typography className="text">
       To access the Members area, please{' '}
       <Link to="/members/login">
         log in
       </Link>.
     </Typography>
   ) : (
-    <Typography className={classes.text}>
+    <Typography className="text">
       Signed in as {authUser.email}
-      <a className={classes.anchor} href="/members" onClick={auth.doSignOut}>
+      <a
+        className="anchor"
+        href="/members"
+        onClick={auth.doSignOut}
+      >
         Sign out
       </a>
     </Typography>
   );
 
-  return <section className={classes.root}>{details}</section>;
+  return <StyledRoot>{details}</StyledRoot>;
 };
-Status.propTypes = propTypes;
-Status.defaultProps = defaultProps;
 
 const StatusWithContext = (props) => (
   <AuthUserContext.Consumer>
