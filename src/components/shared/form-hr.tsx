cf45b603ead1017f58ae9ab9b6515aac7abda158
Mike Mathew <m2mathew@me.com>
2022-11-20 18:58:05 -0600

    chore: switch form-hr to TS and styled-components

---
+++ src/components/shared/form-hr.tsx
@@ -0,0 +1,25 @@
+// External Dependencies
+import clsx from 'clsx';
+import styled from 'styled-components';
+import { FC } from 'react';
+
+// Local Typings
+interface Props {
+  red?: boolean;
+}
+
+// Local Variables
+const StyledHr = styled.hr(({ theme }) => ({
+  '.red': {
+    background: theme.palette.texasFlag.red,
+  },
+  height: 3,
+  background: 'darkred',
+}));
+
+// Component Definition
+const FormHr: FC<Props> = ({ red }) => (
+  <StyledHr className={clsx(red ? 'red' : '')} />
+);
+
+export default FormHr;
