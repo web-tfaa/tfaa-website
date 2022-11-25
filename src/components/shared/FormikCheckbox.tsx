2197895ff7b9e4cf16df9028e3f86395ee85ae62
Mike Mathew <m2mathew@me.com>
2022-11-25 16:52:29 -0600

    chore: add new shared FormikCheckbox component

---
+++ src/components/shared/FormikCheckbox.tsx
@@ -0,0 +1,66 @@
+// External Dependencies
+import {
+  Checkbox,
+  CheckboxProps,
+  FormControl,
+  FormControlLabel,
+} from '@mui/material';
+import { useField } from 'formik';
+import { FC } from 'react';
+import styled from 'styled-components';
+
+// Local Typings
+interface Props extends CheckboxProps {
+  helperText?: string;
+  label: string;
+  name: string;
+}
+
+// Local Variables
+const StyledFormControlLabel = styled(FormControlLabel)(({ theme }) => ({
+  '.label': {
+    marginLeft: theme.spacing(1),
+  },
+}));
+
+// Component Definition
+const FormikCheckbox: FC<Props> = ({
+  inputProps = {},
+  label,
+  name,
+  ...props
+}) => {
+  const [field, meta] = useField(name);
+
+  console.log('field', field);
+
+  const hasError = !!meta.error;
+  const isTouched = !!meta.touched;
+
+  return (
+    <FormControl
+      component="fieldset"
+      error={isTouched && hasError}
+      required
+    >
+      <StyledFormControlLabel
+        classes={{ label: 'label' }}
+        control={(
+          <Checkbox
+            color="primary"
+            {...field}
+            {...props}
+            checked={field.value}
+            inputProps={{
+              ...inputProps,
+            }}
+            onChange={field.onChange}
+          />
+        )}
+        label={label}
+      />
+    </FormControl>
+  );
+};
+
+export default FormikCheckbox;
