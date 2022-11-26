4470e41fb6673ad7133dceb805db425ccf628460
Mike Mathew <m2mathew@me.com>
2022-11-25 18:59:56 -0600

    chore: remove css prop in mobile-nav

--- src/components/nav/mobile-nav.js
+++ src/components/nav/mobile-nav.js
@@ -1,42 +1,60 @@
 // External Dependencies
-// import CardGiftCardIcon from '@mui/icons-material/CardGiftCard';
 import GradeIcon from '@mui/icons-material/Grade';
 import FolderOpenIcon from '@mui/icons-material/FolderOpen';
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
 import React from 'react';
+import styled from 'styled-components';
 
 // Internal Dependencies
 import MobileNavItem from './MobileNavItem';
-import presets, { colors } from '../../utils/presets';
+import presets from '../../utils/presets';
 import { options } from '../../utils/typography';
+
+const StyledRoot = styled.div(({ theme }) => ({
+  [presets.Tablet]: {
+    display: 'none',
+  },
+
+  alignItems: 'center',
+  background: theme.palette.ui.whisper,
+  borderTop: `2px solid ${theme.palette.ui.light}`,
+  bottom: 0,
+  boxSizing: 'border-box',
+  display: 'flex',
+  fontFamily: options.headerFontFamily.join(','),
+  justifyContent: 'space-around',
+  left: 0,
+  paddingBottom: 'env(safe-area-inset-bottom)',
+  position: 'fixed',
+  right: 0,
+  zIndex: 11,
+}));
 
 // Component Definition
 const MobileNav = () => (
-  <div
-    css={{
-      alignItems: 'center',
-      background: colors.ui.whisper,
-      borderTop: `1px solid ${colors.ui.light}`,
-      bottom: 0,
-      display: 'flex',
-      fontFamily: options.headerFontFamily.join(','),
-      justifyContent: 'space-around',
-      left: 0,
-      paddingBottom: 'env(safe-area-inset-bottom)',
-      position: 'fixed',
-      right: 0,
-      zIndex: 11,
-      [presets.Tablet]: {
-        display: 'none',
-      },
-    }}
-  >
-    <MobileNavItem linkTo="/about/" label="About" icon={InfoOutlinedIcon} />
-    <MobileNavItem linkTo="/resources/" label="Resources" icon={FolderOpenIcon} />
-    <MobileNavItem linkTo="/members/" label="Members" icon={PersonOutlinedIcon} />
-    <MobileNavItem linkTo="/sponsors/" label="Sponsors" icon={GradeIcon} />
-  </div>
+  <StyledRoot>
+    <MobileNavItem
+      linkTo="/about/"
+      label="About"
+      icon={InfoOutlinedIcon}
+    />
+    <MobileNavItem
+      linkTo="/resources/"
+      label="Resources"
+      icon={FolderOpenIcon}
+    />
+    <MobileNavItem
+      linkTo="/members/"
+      label="Members"
+      icon={PersonOutlinedIcon}
+    />
+    <MobileNavItem
+      linkTo="/sponsors/"
+      label="Sponsors"
+      icon={GradeIcon}
+    />
+  </StyledRoot>
 );
 
 export default MobileNav;
