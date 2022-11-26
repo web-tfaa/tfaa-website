57d95522c9a18d09e0690cdd80ba140485991a1d
Mike Mathew <m2mathew@me.com>
2022-11-26 12:49:18 -0600

    refactor: for featured-stat, convert index to TS, remove css props from index and stat.js

--- src/components/shared/featured-stat/index.tsx
+++ src/components/shared/featured-stat/index.tsx
@@ -1,11 +1,12 @@
 // External Dependencies
-import React from 'react';
+import React, { FC } from 'react';
 import Face from '@mui/icons-material/Face';
 import LocationCity from '@mui/icons-material/LocationCity';
 import School from '@mui/icons-material/School';
+import styled from 'styled-components';
 
 // Internal Dependencies
-import presets, { colors } from '../../../utils/presets';
+import presets from '../../../utils/presets';
 import Stat from './stat';
 
 // Icon colors
@@ -15,26 +16,27 @@ const schoolIconColor = '#f9b320';
 const faceIconColor = '#b96af7';
 
 // Local Variables
+const StyledRoot = styled.div(({ theme }) => ({
+  [presets.Tablet]: {
+    marginBottom: 0,
+  },
+
+  backgroundColor: `${theme.palette.ui.light}`,
+  display: 'flex',
+  flex: '1 1 100%',
+  flexWrap: 'wrap',
+  justifyContent: 'space-around',
+  marginBottom: 68,
+  padding: '2.5em',
+  textAlign: 'center',
+}));
+
 const numberOfDistricts = 1246;
 const numberOfSchools = 3000;
 
 // Component Definition
-const FeaturedStat = () => (
-  <div
-    css={{
-      backgroundColor: `${colors.ui.light}`,
-      display: 'flex',
-      flex: '1 1 100%',
-      flexWrap: 'wrap',
-      justifyContent: 'space-around',
-      marginBottom: 68,
-      padding: '2.5em',
-      textAlign: 'center',
-      [presets.Tablet]: {
-        marginBottom: 0,
-      },
-    }}
-  >
+const FeaturedStat: FC = () => (
+  <StyledRoot>
     <Stat
       color={queueMusicIconColor}
       title
@@ -59,7 +61,7 @@ const FeaturedStat = () => (
     >
       <div>5+ Million Students</div>
     </Stat>
-  </div>
+  </StyledRoot>
 );
 
 export default FeaturedStat;
