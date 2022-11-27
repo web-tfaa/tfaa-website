69da23c620a68f29157f2862b635950e5db0be34
Mike Mathew <m2mathew@me.com>
2022-11-27 13:43:29 -0600

    fix(footer): update footer link styles to match production

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -8,50 +8,60 @@ import { options } from '../../utils/typography';
 import { currentYearLong } from '../../utils/helpers';
 
 // Local Variables
-const StyledRoot = styled.footer({
-  '.footer-left': {
-    maxWidth: '60%',
+const StyledRoot = styled.footer(({ theme }) => {
+  const linkStyles = {
+    color: 'inherit',
+    textDecoration: 'none',
+    transition: `all ${presets.animation.speedFast} ${presets.animation.curveDefault}`,
+    borderBottom: `1px solid ${theme.palette.ui.bright}`,
+    boxShadow: `inset 0 -2px 0px 0px ${theme.palette.ui.bright}`,
+    fontFamily: options.headerFontFamily.join(','),
+    fontWeight: 'bold',
+    marginLeft: 5,
 
-    '& a': {
-      marginLeft: 5,
-
-      '&:visited': {
-        color: 'black',
-      },
+    '&:hover': {
+      background: theme.palette.ui.bright,
     },
-  },
 
-  '.footer-right': {
-    textAlign: 'right',
-
-    '& a': {
-      marginLeft: 5,
-
-      '&:visited': {
-        color: 'black',
-      },
+    '&:visited': {
+      color: 'black',
     },
-  },
+  };
 
-  [presets.Tablet]: {
-    fontSize: 16,
-    padding: '2em',
-    position: 'static',
-  },
-  alignItems: 'center',
-  background: '#fbfafc',
-  borderTop: '4px solid #2D456F',
-  bottom: 68,
-  boxShadow: '-2px 0 5px #444',
-  boxSizing: 'border-box',
-  display: 'flex',
-  flex: 1,
-  fontFamily: options.headerFontFamily.join('/'),
-  fontSize: 12,
-  justifyContent: 'space-between',
-  padding: '1.25em',
-  position: 'relative',
-  zIndex: 10,
+  return {
+    '.footer-left': {
+      maxWidth: '60%',
+
+      '& a': linkStyles,
+    },
+
+    '.footer-right': {
+      textAlign: 'right',
+
+      '& a': linkStyles,
+    },
+
+    [presets.Tablet]: {
+      fontSize: 16,
+      padding: '2em',
+      position: 'static',
+    },
+
+    alignItems: 'center',
+    background: '#fbfafc',
+    borderTop: '4px solid #2D456F',
+    bottom: 68,
+    boxShadow: '-2px 0 5px #444',
+    boxSizing: 'border-box',
+    display: 'flex',
+    flex: 1,
+    fontFamily: options.headerFontFamily.join('/'),
+    fontSize: 12,
+    justifyContent: 'space-between',
+    padding: '1.25em',
+    position: 'relative',
+    zIndex: 10,
+  };
 });
 
 // Component Definition
@@ -63,6 +73,7 @@ const Footer: FC = () => (
         Texas Music Administrators Conference
       </a>
     </div>
+
     <div className="footer-right">
       <div>
         Built by
