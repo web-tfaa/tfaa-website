0a368dc37f4662609ff87bddbd0a35cc61cb91a5
Mike Mathew <m2mathew@me.com>
2022-11-26 22:01:53 -0600

    refactor: in sponsor-info, remove css props

--- src/pages/sponsors/sponsor-info.js
+++ src/pages/sponsors/sponsor-info.js
@@ -3,6 +3,7 @@ import { Box } from '@mui/material';
 import { Helmet } from 'react-helmet';
 import PropTypes from 'prop-types';
 import React from 'react';
+import styled from 'styled-components';
 
 // Internal Dependencies
 import { isTodayAfterJune30th } from '../../utils/helpers';
@@ -19,111 +20,106 @@ import Status from '../members/status';
 import MobileDivider from '../../components/shared/MobileDivider';
 
 // Local Variables
-const boldStyles = { fontWeight: 600 };
+const StyledRoot = styled.div(({ $isAuthenticated }) => ({
+  '.strong': {
+    fontWeight: 600,
+  },
+
+  [presets.Tablet]: {
+    paddingLeft: !$isAuthenticated ? '1.5rem' : 0,
+  },
+
+  paddingLeft: 0,
+  width: '0 auto',
+}));
 
 // Component Definition
-const SponsorInfo = (props) => {
-  const {
-    isAuthenticated,
-    location,
-  } = props;
+const SponsorInfo = ({
+  isAuthenticated,
+  location,
+}) => (
+  <Layout location={location}>
+    <StyledRoot $isAuthenticated={isAuthenticated}>
+      <Status />
 
-  return (
-    <Layout location={location}>
-      <div
-        css={{
-          paddingLeft: 0,
-          width: '0 auto',
-          [presets.Tablet]: {
-            paddingLeft: !isAuthenticated ? '1.5rem' : 0,
-          },
-        }}
-      >
-        <Status />
+      <Container>
+        <Helmet>
+          <title>TMAC | Sponsor Information</title>
+        </Helmet>
 
-        <Container>
-          <Helmet>
-            <title>TMAC | Sponsor Information</title>
-          </Helmet>
+        <Box
+          display="flex"
+          flexDirection="column"
+          padding={4}
+        >
+          <CardHeadline>Sponsor TMAC</CardHeadline>
 
-          <div
-            css={{
-              display: 'flex',
-              flexDirection: 'column',
-              padding: 32,
-            }}
-          >
-            <CardHeadline>Sponsor TMAC</CardHeadline>
+          {!isTodayAfterJune30th
+            ? (
+              <Box mt={3}>
+                <EnhancedAlert title="Sponsorship Notice">
+                  TMAC Sponsorship will open up again on July 1st.
+                </EnhancedAlert>
+              </Box>
+            ) : (
+              <>
+                <FuturaDiv>
+                  To become a TMAC sponsor please complete these three steps:
+                </FuturaDiv>
 
-            {!isTodayAfterJune30th
-              ? (
-                <Box mt={3}>
-                  <EnhancedAlert title="Sponsorship Notice">
-                    TMAC Sponsorship will open up again on July 1st.
-                  </EnhancedAlert>
-                </Box>
-              ) : (
-                <>
-                  <FuturaDiv>
-                    To become a TMAC sponsor please complete these three steps:
-                  </FuturaDiv>
-
-                  <FuturaDiv>
-                    <span css={boldStyles}>
-                      1.{' '}
-                    </span>
-                    Sign up for a TMAC website login.
-                  </FuturaDiv>
-
-                  <FuturaDiv>
-                    <span css={boldStyles}>
-                      2.{' '}
-                    </span>
-                    Complete the Sponsorship Form.
-                  </FuturaDiv>
-
-                  <FuturaDiv>
-                    <span css={boldStyles}>
-                      3.{' '}
-                    </span>
-                    To pay, mail invoice with check to the TMAC Treasurer.
-                  </FuturaDiv>
-                </>
-              )}
-          </div>
-
-          {isTodayAfterJune30th && (
-            <>
-              <div
-                css={{
-                  display: 'flex',
-                  justifyContent: 'flex-end',
-                }}
-              >
-                <CtaButton
-                  buttonColor="blue"
-                  to="/sponsors/register"
-                  state={{ level: location && location.state && location.state.level }}
-                >
-                  <span>
-                    Begin Sponsorship
+                <FuturaDiv>
+                  <span className="strong">
+                    1.{' '}
                   </span>
-                  <ArrowForwardIcon />
-                </CtaButton>
-              </div>
+                  Sign up for a TMAC website login.
+                </FuturaDiv>
 
-              <div style={{ marginTop: '1.5rem' }}>
-                * Sponsorship is not complete until payment is received.
-              </div>
-            </>
-          )}
-        </Container>
+                <FuturaDiv>
+                  <span className="strong">
+                    2.{' '}
+                  </span>
+                  Complete the Sponsorship Form.
+                </FuturaDiv>
 
-        <MobileDivider />
-      </div>
-    </Layout>
-  );
-};
+                <FuturaDiv>
+                  <span className="strong">
+                    3.{' '}
+                  </span>
+                  To pay, mail invoice with check to the TMAC Treasurer.
+                </FuturaDiv>
+              </>
+            )}
+        </Box>
+
+        {isTodayAfterJune30th && (
+        <>
+          <Box
+            display="flex"
+            justifyContent="flex-end"
+          >
+            <CtaButton
+              buttonColor="blue"
+              to="/sponsors/register"
+              state={{ level: location && location.state && location.state.level }}
+            >
+              <span>
+                Begin Sponsorship
+              </span>
+              <ArrowForwardIcon />
+            </CtaButton>
+          </Box>
+
+          <div style={{ marginTop: '1.5rem' }}>
+            * Sponsorship is not complete until payment is received.
+          </div>
+        </>
+        )}
+      </Container>
+
+      <MobileDivider />
+    </StyledRoot>
+  </Layout>
+);
 
 SponsorInfo.propTypes = {
   isAuthenticated: PropTypes.bool.isRequired,
