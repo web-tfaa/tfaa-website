e0f0e1bea16ea19616381ba784e3fae26650ae45
Mike Mathew <m2mathew@me.com>
2022-12-11 23:35:45 -0600

    refactor(home): add Greeting section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,8 @@ import { Helmet } from 'react-helmet';
 import styled from 'styled-components';
 
 // Internal Dependencies
-import FeaturedStat from '../components/shared/featured-stat';
 import Layout from '../components/layout';
+import Greetings from '../components/home/Greetings';
 import HomeBanner from '../components/home/HomeBanner';
 import TakeAction from '../components/home/TakeAction';
 
@@ -22,25 +22,6 @@ const StyledRoot = styled.div({
   justifyContent: 'space-between',
 });
 
-// const BlogPost = ({node}) => {
-//   return (
-//     <div style={{
-//       marginBottom: '1.5rem',
-//       padding: '1.5rem',
-//       border: '1px solid #ccc'
-//     }}>
-//       <h3><Link to={node.slug}>{node.title}</Link></h3>
-//       <p>{node.createdAt}</p>
-//       <div>
-//         <div>
-//           <Img resolutions={node.featuredImage.resolutions}/>
-//         </div>
-//         <div>{node.content.childMarkdownRemark.excerpt}</div>
-//       </div>
-//     </div>
-//   )
-// }
-
 // Component Definition
 const Home: FC<Props> = ({ location }) => (
   <Layout location={location}>
@@ -53,9 +34,8 @@ const Home: FC<Props> = ({ location }) => (
 
       <TakeAction />
 
-      <FeaturedStat />
-      {/* {props.data.allContentfulBlog.edges.map((edge) =>
-        <BlogPost key={edge.node.id} node={edge.node} />)} */}
+      <Greetings />
+
     </StyledRoot>
   </Layout>
 );
