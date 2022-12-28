1496175f566f9bd12ec02d7b4da027e75b85545a
Mike Mathew <m2mathew@me.com>
2022-12-28 07:19:22 -0600

    refactor(home): in FineArtsPriority, adjust for all screen sizes

--- src/components/home/FineArtsPriority.tsx
+++ src/components/home/FineArtsPriority.tsx
@@ -11,20 +11,26 @@ import { appNameShort } from '../../utils/app-constants';
 const StyledRoot = styled.section(({ theme }) => ({
   '.linkContainer a': {
     [theme.breakpoints.down('mobile')]: {
-      fontSize: 15,
+      display: 'none',
     },
     fontSize: 16,
     fontWeight: theme.typography.fontWeightRegular,
   },
 
   '.priorityContent': {
+    [theme.breakpoints.down('lg')]: {
+      padding: theme.spacing(6, 8, 6, 10),
+      width: '55%',
+    },
     [theme.breakpoints.down('mobile')]: {
       padding: theme.spacing(4, 10, 4, 4),
-      width: '70%',
+      width: '100%',
+      position: 'relative',
+      top: 0,
+      left: 0,
     },
     [theme.breakpoints.down('sm')]: {
-      padding: theme.spacing(4),
-      width: '85%',
+      padding: theme.spacing(2, 4),
     },
 
     padding: theme.spacing(8, 15, 8, 8),
@@ -35,37 +41,45 @@ const StyledRoot = styled.section(({ theme }) => ({
   },
 
   '.redMotif': {
-    [theme.breakpoints.down('sm')]: {
-      height: 100,
-      right: -220,
-      top: -44,
-      width: 360,
+    [theme.breakpoints.down('lg')]: {
+      right: -64,
+      top: -64,
     },
+    [theme.breakpoints.down('mobile')]: {
+      height: 100,
+      right: -200,
+      top: -4,
+    },
+
     height: 200,
-    position: 'relative',
-    right: -660,
-    top: -104,
+    position: 'absolute',
+    right: -20,
+    top: -44,
     width: 494,
   },
 
   '.tealMotif': {
-    [theme.breakpoints.down('sm')]: {
-      height: 400,
-      left: 140,
-      top: -44,
-      width: 240,
+    [theme.breakpoints.down('mobile')]: {
+      height: 500,
+      left: -264,
+      top: -60,
     },
+
     height: 533,
-    position: 'relative',
+    position: 'absolute',
     left: -240,
     top: -54,
     width: 335,
   },
 
   h4: {
+    [theme.breakpoints.down('lg')]: {
+      fontSize: 40,
+    },
     [theme.breakpoints.down('mobile')]: {
-      fontSize: 36,
+      fontSize: 30,
       marginBottom: theme.spacing(1.5),
+      maxWidth: '60%',
     },
     [theme.breakpoints.down('sm')]: {
       fontSize: 28,
@@ -88,7 +102,10 @@ const StyledRoot = styled.section(({ theme }) => ({
     backgroundSize: 'cover',
   },
   [theme.breakpoints.down('mobile')]: {
+    backgroundSize: 'auto',
     padding: theme.spacing(4, 0, 2),
+    overflow: 'scroll',
+    alignItems: 'flex-end',
   },
 
   background: "url('https://res.cloudinary.com/tmac/image/upload/v1671285382/symphony-orchestra-performing-on-stage.png') no-repeat",
@@ -99,7 +116,11 @@ const StyledRoot = styled.section(({ theme }) => ({
   justifyContent: 'flex-end',
   overflow: 'hidden',
   padding: theme.spacing(5, 0, 2.5),
+  position: 'relative',
   width: '100%',
+  top: 0,
+  left: 0,
+  height: 614,
 }));
 
 // Component Definition
