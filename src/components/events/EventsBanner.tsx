bf418afcebb8818d866a910a5c1a5f7ee524e149
Mike Mathew <m2mathew@me.com>
2023-01-29 13:59:11 -0600

    feat(events): adjust EventsBanner for small screens

--- src/components/events/EventsBanner.tsx
+++ src/components/events/EventsBanner.tsx
@@ -8,51 +8,6 @@ import { appName } from '../../utils/app-constants';
 
 // Local Variables
 const StyledRoot = styled.section(({ theme }) => ({
-  // '.bannerLeft': {
-  //   a: {
-  //     [theme.breakpoints.down('lg')]: {
-  //       fontSize: 18
-  //     },
-  //     [theme.breakpoints.down('mobile')]: {
-  //       fontSize: 16
-  //     },
-  //     color: theme.palette.common.white,
-  //     fontWeight: theme.typography.fontWeightMedium,
-  //   },
-
-  //   [theme.breakpoints.down('lg')]: {
-  //     paddingRight: theme.spacing(2),
-  //   },
-  //   [theme.breakpoints.down('mobile')]: {
-  //     order: 2,
-  //   },
-
-  //   maxWidth: 400,
-  //   paddingRight: theme.spacing(3),
-  // },
-
-  // '.bannerRight': {
-  //   [theme.breakpoints.down('mobile')]: {
-  //     order: 1,
-  //     marginBottom: theme.spacing(1),
-  //   },
-  // },
-
-  // '#bannerBackgroundVideo': {
-  //   [theme.breakpoints.down('lg')]: {
-  //     display: 'none',
-  //   },
-  //   width: '100%',
-  //   height: '100%',
-  //   objectFit: 'cover',
-  //   position: 'fixed',
-  //   left: 0,
-  //   right: 0,
-  //   top: 0,
-  //   bottom: 0,
-  //   zIndex: -1,
-  // },
-
   'h1, h3, p': {
     color: theme.palette.common.white,
     letterSpacing: 0,
@@ -61,12 +16,12 @@ const StyledRoot = styled.section(({ theme }) => ({
   },
 
   h1: {
-    [theme.breakpoints.down('lg')]: {
+    [theme.breakpoints.down('md')]: {
       fontSize: 64,
       lineHeight: '64px',
     },
     [theme.breakpoints.down('mobile')]: {
-      fontSize: 42,
+      fontSize: 36,
       lineHeight: '42px',
     },
     fontSize: 82,
@@ -81,42 +36,19 @@ const StyledRoot = styled.section(({ theme }) => ({
   },
 
   p: {
+    [theme.breakpoints.down('md')]: {
+      maxWidth: '75%',
+    },
+    [theme.breakpoints.down('mobile')]: {
+      maxWidth: '90%',
+    },
     fontSize: 20,
     margin: theme.spacing(8, 0),
     maxWidth: '50%',
     textAlign: 'center',
   },
-  // h2: {
-  //   [theme.breakpoints.down('lg')]: {
-  //     fontSize: 32,
-  //     lineHeight: '32px',
-  //   },
-  //   [theme.breakpoints.down('mobile')]: {
-  //     fontSize: 24,
-  //     lineHeight: '24px',
-  //     marginBottom: theme.spacing(8),
-  //   },
-  //   fontSize: 40,
-  //   fontWeight: 900,
-  //   lineHeight: '40px',
-  // },
-
-  // '& > div': {
-  //   [theme.breakpoints.down('mobile')]: {
-  //     alignItems: 'flex-start',
-  //     justifyContent: 'flex-start',
-  //     width: '100%',
-  //   },
-
-  //   display: 'flex',
-  //   justifyContent: 'center',
-  //   width: '50%',
-  // },
 
   '&::before': {
-    [theme.breakpoints.down('mobile')]: {
-      opacity: 0.3,
-    },
     content: '""',
     display: 'block',
     backgroundColor: theme.palette.tfaa.backgroundDark,
@@ -128,29 +60,18 @@ const StyledRoot = styled.section(({ theme }) => ({
     width: '100%',
   },
 
-  // [theme.breakpoints.down('lg')]: {
-  //   background: 'url("https://res.cloudinary.com/tmac/image/upload/v1670299970/tfaa-banner-video-poster--tablet.png") no-repeat',
-  //   backgroundSize: 'cover',
-  //   gap: 0,
-  //   height: 600,
-  //   padding: theme.spacing(6),
-  //   position: 'static',
-  // },
   [theme.breakpoints.down('mobile')]: {
     background: 'url("https://res.cloudinary.com/tmac/image/upload/v1675017739/people-in-audience-mobile.png") no-repeat',
-    backgroundSize: 'contain',
-    height: 540,
-    padding: theme.spacing(4),
-    flexDirection: 'column',
+    backgroundSize: 'cover',
+    height: '100%',
+    padding: theme.spacing(8, 4),
   },
 
   alignItems: 'center',
   background: 'url("https://res.cloudinary.com/tmac/image/upload/v1675017739/people-in-audience.png") no-repeat',
   display: 'flex',
-  // gap: theme.spacing(8),
   height: 720,
   flexDirection: 'column',
-  // justifyContent: 'center',
   padding: theme.spacing(8),
   position: 'relative',
   width: '100%',
