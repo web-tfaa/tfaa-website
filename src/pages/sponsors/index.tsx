a6c935432146c9f8fc4ee8cd080a882f14f532f5
Mike Mathew <m2mathew@me.com>
2023-01-31 19:06:22 -0600

    fix(sponsors): add back accidentally-deleted sponsors page

--- src/pages/sponsors/index.tsx
+++ src/pages/sponsors/index.tsx
@@ -5,9 +5,9 @@ import styled from 'styled-components';
 // Internal Dependencies
 import { ReCaptchaProvider } from '../../components/shared/ReCaptchaProvider';
 import Layout from '../../components/layout';
-import MembersBanner from '../../components/members/MembersBanner';
-import MembersInfoBanner from '../../components/members/MembersInfoBanner';
-import MembersOneTwoThree from '../../components/members/MembersOneTwoThree';
+import SponsorsBanner from '../../components/sponsors/SponsorsBanner';
+import SponsorsHeroBannerImage from '../../components/sponsors/SponsorsHeroBannerImage';
+import SponsorsList from '../../components/sponsors/SponsorsList';
 import WhereWeHaveBeen from '../../components/about/WhereWeHaveBeen';
 
 // Local Typings
@@ -25,31 +25,25 @@ const StyledRoot = styled.div({
 });
 
 // Component Definition
-const Members: React.FC<Props> = ({ location }) => {
-  const nonMemberContent = React.useMemo(() => (
-    <>
-      <MembersBanner />
-
-      <MembersInfoBanner />
-
-      <MembersOneTwoThree />
-
-      <WhereWeHaveBeen color="membership" />
-    </>
-  ), []);
-
+const Sponsors: React.FC<Props> = ({ location }) => {
   return (
     <Layout
       location={location}
-      pageTitle="Members"
+      pageTitle="Sponsors"
     >
       <ReCaptchaProvider>
         <StyledRoot>
-          {nonMemberContent}
+          <SponsorsBanner />
+
+          <SponsorsHeroBannerImage />
+
+          <SponsorsList />
+
+          <WhereWeHaveBeen color="membership" />
         </StyledRoot>
       </ReCaptchaProvider>
     </Layout>
   );
 };
 
-export default Members;
+export default Sponsors;
