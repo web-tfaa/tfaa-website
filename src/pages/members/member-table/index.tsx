2ea8f5034ed6480717e871d91725a537c909faa2
Mike Mathew <m2mathew@me.com>
2023-07-23 17:35:13 -0500

    refactor(members): remove uglifyEmail work to show emails correctly

--- src/pages/members/member-table/index.tsx
+++ src/pages/members/member-table/index.tsx
@@ -5,6 +5,7 @@ import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
+// import { DataGrid, GridColDef, GridRowsProp } from '@mui/x-data-grid';
 import React, { FC, useState } from 'react';
 import styled from 'styled-components';
 
@@ -134,6 +135,17 @@ const MemberTable: FC<Props> = ({
   if (!data) {
     return null;
   }
+
+  // const columns: GridColDef[] = [
+  //   { field: 'col1', headerName: 'Column 1', width: 150 },
+  //   { field: 'col2', headerName: 'Column 2', width: 150 },
+  // ];
+
+  // const rows: GridRowsProp = [
+  //   { id: 1, col1: 'Hello', col2: 'World' },
+  //   { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
+  //   { id: 3, col1: 'MUI', col2: 'is Amazing' },
+  // ];
 
   return (
     <StyledRoot variant="outlined">
