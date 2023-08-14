e09b46e871190a165eb7080462658f845c732fd1
Mike Mathew <m2mathew@me.com>
2023-08-14 11:04:26 -0500

    refactor: change Twitter/X to only "X"

--- src/components/footer/FooterFollowUs.tsx
+++ src/components/footer/FooterFollowUs.tsx
@@ -38,7 +38,7 @@ const FooterFollowUs: FC = () => {
           rel="noreferrer noopener"
           target="_blank"
         >
-          Twitter/X
+          X
         </a>
       </Box>
     </div>
